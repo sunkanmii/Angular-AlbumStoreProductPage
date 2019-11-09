10abea0bc2e6a6da405f4925dfba0eeecfba28a4
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-11-09 23:21:17 +0100

    Corrected errors made in part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -24,8 +24,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => (<Album>)response.json());
   }
 
-  getProducts(){
-    return (Observable<Product[]>)this._http.get(this._productsUrl).map((response) => (<Product[]>)response.json());
-  }
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) => (<Product[]>)response.json());
 
 }
