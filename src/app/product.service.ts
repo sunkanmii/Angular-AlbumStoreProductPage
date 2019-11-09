58300f946ffabebb65491639e2435fe47b0cba59
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-11-09 19:40:18 +0100

    Corrected errors made in step 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import 'rxjx/add/operator/map';
+
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class ProductService {
