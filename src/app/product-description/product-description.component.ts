759d81d6b178768d6a08b2cbe19006f8ca95428e
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-11-09 21:06:28 +0100

    Corrected errors made in task 4

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from  '../product.service';
 
-
-import { Album } from "../album";
+import { Album } from "./album";
 
 @Component({
   selector: 'app-product-description',
