5e599065187c2337ab756c45489e95845502b442
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-11-09 23:54:49 +0100

    Corrected errors made in part 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const appRoutes: Routes[
+const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'product/:id', componenet: ProductPageComponent },
   { path: '', redirect: 'products', pathMatch: 'full' }
